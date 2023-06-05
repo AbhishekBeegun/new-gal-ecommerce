ab6e4172b8b31bd950dcaec39b83d826f3820ec8
Dooshyant Beegun <abhishekbeegun0642@gmail.com>
2023-06-05 13:32:35 +0400

    first-commit

---
+++ src/components/Banners/Vertical.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Vertical = () => {
+  return (
+    <div>Vertical</div>
+  )
+}
+
+export default Vertical
---
+++ src/components/Categories/CategorieCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CategorieCard = () => {
+  return (
+    <div>CategorieCard</div>
+  )
+}
+
+export default CategorieCard
